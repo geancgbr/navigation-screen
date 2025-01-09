13ac2118a7deb16d7620cfd22c923839c3bd34c1
geancarlos <geangbr@gmail.com>
2025-01-08 23:24:29 -0300

    desenvolvendo o text

--- app/src/main/java/br/com/gean/navigation/screens/LoginScreen.kt
+++ app/src/main/java/br/com/gean/navigation/screens/LoginScreen.kt
@@ -1,2 +1,39 @@
 package br.com.gean.navigation.screens
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 
+@Composable
+fun LoginScreen(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFF9E3ADB))
+    ) {
+        Column {
+            Column {
+                Text(
+                    text = "Login Screen",
+                    fontSize = 48.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Magenta,
+                )
+                Text(text = "Login Screen")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun LoginScreenPreview() {
+    LoginScreen()
+}
